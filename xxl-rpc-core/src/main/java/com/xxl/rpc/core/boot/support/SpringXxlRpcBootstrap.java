3ff1c004e09e228bf52194af7905b7199f9f88d9
xuxueli <931591021@qq.com>
2025-01-19 03:34:02 +0800

    Update RM

--- xxl-rpc-core/src/main/java/com/xxl/rpc/core/boot/support/SpringXxlRpcBootstrap.java
+++ xxl-rpc-core/src/main/java/com/xxl/rpc/core/boot/support/SpringXxlRpcBootstrap.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -21,7 +21,7 @@ import java.util.List;
  *
  * @author xuxueli 2024-12-21
  */
-public class SpringXxlRpcBootstrap extends XxlRpcBootstrap implements ApplicationContextAware, SmartInitializingSingleton, DisposableBean, InstantiationAwareBeanPostProcessor {
+public class SpringXxlRpcBootstrap extends XxlRpcBootstrap implements ApplicationContextAware, SmartInitializingSingleton, DisposableBean, SmartInstantiationAwareBeanPostProcessor {
     private static final Logger logger = LoggerFactory.getLogger(SpringXxlRpcBootstrap.class);
 
     @Override
